d1e539ae504a336bb9d3745b634c72f74ce7f0dd
Steven Indzeoski <steven.indzeoski@me.com>
2016-04-10 23:19:46 -0600

    Can now mouseover card names for the image, on a touch screen tap once to view and tap twice to follow link. Still have a few kinks to work out.

--- ghost-mtg.js
+++ ghost-mtg.js
@@ -3,6 +3,8 @@
   'use strict';
 
   var DECK_LI_REGEX = /^(\d+)\s(.+)$/;
+  var GATHERER_IMAGE_URL = 'http://gatherer.wizards.com/Handlers/Image.ashx' +
+      '?type=card';
   var GATHERER_SEARCH_URL = 'http://gatherer.wizards.com/pages/search/' +
       'default.aspx';
 
@@ -11,14 +13,44 @@
     return;
   }
 
-  $('.decklist li').each(function() {
+  $('.decklist li').each(function(index) {
     var li = $(this).html();
     var result = DECK_LI_REGEX.exec(li);
     var num = result[1];
     var cardName = result[2];
     var param = 'name=' + encodeURIComponent('+["' + cardName + '"]');
     var cardLink = GATHERER_SEARCH_URL + '?' + param;
+
     $(this).html(num + ' <a href="' + cardLink + '" target="_blank">' +
         cardName + '</a>');
+    $(this).attr('data-name', cardName);
+
+    if (index === 0) {
+      $('.decklist__image').append('<img src="' + GATHERER_IMAGE_URL +
+          '&name=' + encodeURIComponent(cardName) + '">');
+    }
+  });
+
+  $('.decklist li').mouseover(function() {
+    var name = $(this).attr('data-name');
+    var src = GATHERER_IMAGE_URL + '&name=' + encodeURIComponent(name);
+
+    $('.decklist__image img').attr('src', src);
+  });
+
+  $('.decklist li').on('touchstart', function() {
+    if (!$(this).attr('data-touched')) {
+      $('.decklist li').removeAttr('data-touched');
+      $(this).attr('data-touched', 'touched');
+    }
+  });
+
+  $('.decklist a').click(function(event) {
+    if ($(this).parent().attr('data-touched') === 'touched') {
+      event.preventDefault();
+      $(this).parent().attr('data-touched', 'hover');
+    } else {
+      $(this).parent().removeAttr('data-touched');
+    }
   });
 })(jQuery);
